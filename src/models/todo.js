a61ead7217497cc3e42ec5d23342be4e51c8dfe1
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-10-28 21:34:24 -0400

    add: create function on models

--- src/models/todo.js
+++ src/models/todo.js
@@ -8,6 +8,14 @@ const getAll = async () => {
   return todoArray;
 };
 
+const create = async (todo) => {
+  const { task, status } = todo;
+  const db = await connection(); 
+  const result = await db.collection(TODO).insertOne({ task, status });
+  return result;
+};
+
 module.exports = {
   getAll,
+  create,
 };
